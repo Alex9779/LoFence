f4951a286e42ca14a3e411b0eb58eb0698c1a015
Alexander Leisentritt <alexander.leisentritt@alitecs.de>
2023-10-18 19:53:33 +0200

    implement decoding settings

--- decoder/Chirpstack v3/decoder.js
+++ decoder/Chirpstack v3/decoder.js
@@ -6,6 +6,15 @@ function Decode(fPort, bytes, variables) {
     decoded.fence_positive = (bytes[2] << 8) | bytes[3];
     decoded.fence_negative = (bytes[4] << 8) | bytes[5];
   }
+  else if (fPort === 2 && bytes.length === 7) {
+    decoded.tdc = (bytes[0] << 12) | (bytes[1] << 8) | bytes[2];
+    decoded.msr_ms = (bytes[3] << 8) | bytes[4];
+    decoded.max_volt = (bytes[5] << 8) | bytes[6];
+  }
+  else if (fPort === 3 && bytes.length === 3) {
+    decoded.bat_low = (bytes[0] << 8) | bytes[1];
+    decoded.bat_low_count_max = bytes[2];
+  }
 
   return decoded;
 }
--- decoder/TTN/decoder.js
+++ decoder/TTN/decoder.js
@@ -1,9 +1,28 @@
 function decodeUplink(input) {
-  return {
-    data: {
-      battery: (input.bytes[0] << 8) | input.bytes[1],
-      fence_positive: (input.bytes[2] << 8) | input.bytes[3],
-      fence_negative: (input.bytes[4] << 8) | input.bytes[5],
-    },
-  };
+  if (input.fPort == 1) {
+    return {
+      data: {
+        battery: (input.bytes[0] << 8) | input.bytes[1],
+        fence_positive: (input.bytes[2] << 8) | input.bytes[3],
+        fence_negative: (input.bytes[4] << 8) | input.bytes[5],
+      },
+    };
+  }
+  else if (input.fPort == 2) {
+    return {
+      data: {
+        tdc: (input.bytes[0] << 12) | (input.bytes[1] << 8) | input.bytes[2],
+        msr_ms: (input.bytes[3] << 8) | input.bytes[4],
+        max_volt: (input.bytes[5] << 8) | input.bytes[6],
+      },
+    };
+  }
+  else if (input.fPort == 3) {
+    return {
+      data: {
+        bat_low: (input.bytes[0] << 8) | input.bytes[1],
+        bat_low_count_max: input.bytes[2],
+      },
+    };
+  }
 }
