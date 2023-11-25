c8edfff1110f54cbb106eda3e55db3c571ef7100
Alexander Leisentritt <alexander.leisentritt@alitecs.de>
2023-11-25 11:36:11 +0100

    change object creation

--- decoder/Chirpstack v4/decoder.js
+++ decoder/Chirpstack v4/decoder.js
@@ -1,31 +1,25 @@
 function decodeUplink(input) {
+  var data = {};
+
   if (input.fPort == 1) {
-    return {
-      data: {
-        battery: (input.bytes[0] << 8) | input.bytes[1],
-        fence_positive: (input.bytes[2] << 8) | input.bytes[3],
-        fence_negative: (input.bytes[4] << 8) | input.bytes[5],
-      },
-    };
+    data.battery = (input.bytes[0] << 8) | input.bytes[1];
+    data.fence_positive = (input.bytes[2] << 8) | input.bytes[3];
+    data.fence_negative = (input.bytes[4] << 8) | input.bytes[5];
   }
   else if (input.fPort == 2) {
-    return {
-      data: {
-        version: input.bytes[0],
-        tdc: (input.bytes[1] << 12) | (input.bytes[2] << 8) | input.bytes[3],
-        msr_ms: (input.bytes[4] << 8) | input.bytes[5],
-        max_volt: (input.bytes[6] << 8) | input.bytes[7],
-        bat_low_min: ((input.bytes[4] << 8) | input.bytes[5]).toString() + " mV",
-      },
-    };
+    data.version = input.bytes[0];
+    data.tdc = (input.bytes[1] << 12) | (input.bytes[2] << 8) | input.bytes[3];
+    data.msr_ms = (input.bytes[4] << 8) | input.bytes[5];
+    data.max_volt = (input.bytes[6] << 8) | input.bytes[7];
+    data.bat_low_min = ((input.bytes[4] << 8) | input.bytes[5]).toString() + " mV";
   }
   else if (input.fPort == 3) {
-    return {
-      data: {
-        version: input.bytes[0],
-        bat_low: (input.bytes[1] << 8) | input.bytes[2],
-        bat_low_count_max: input.bytes[3],
-      },
-    };
+    data.version = input.bytes[0],
+    data.bat_low = (input.bytes[1] << 8) | input.bytes[2],
+    data.bat_low_count_max = input.bytes[3],
+    data.latitude = parseFloat(input.variables[latitude]),
+    data.longitude = parseFloat(input.variables[longitude]);
   }
+
+  return { data };
 }
