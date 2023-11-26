acb9c512792a0e5877ce64537b30d0249b4ec7e1
Alexander Leisentritt <alexander.leisentritt@alitecs.de>
2023-11-26 18:51:12 +0100

    fix decoders

--- decoder/Chirpstack v4/decoder_loc.js
+++ decoder/Chirpstack v4/decoder_loc.js
@@ -7,18 +7,16 @@ function decodeUplink(input) {
     data.fence_negative = (input.bytes[4] << 8) | input.bytes[5];
   }
   else if (input.fPort == 2) {
-    data.version = input.bytes[0];
-    data.tdc = (input.bytes[1] << 12) | (input.bytes[2] << 8) | input.bytes[3];
-    data.msr_ms = (input.bytes[4] << 8) | input.bytes[5];
-    data.max_volt = (input.bytes[6] << 8) | input.bytes[7];
-    data.bat_low_min = ((input.bytes[4] << 8) | input.bytes[5]).toString() + " mV";
+    data.version = (input.bytes[0]).toString();
+    data.tdc = ((input.bytes[1] << 12) | (input.bytes[2] << 8) | input.bytes[3]).toString() + " s";
+    data.msr_ms = ((input.bytes[4] << 8) | input.bytes[5]).toString() + " ms";
+    data.max_volt = ((input.bytes[6] << 8) | input.bytes[7]).toString() + " V";
   }
   else if (input.fPort == 3) {
-    data.version = input.bytes[0],
-    data.bat_low = (input.bytes[1] << 8) | input.bytes[2],
-    data.bat_low_count_max = input.bytes[3],
-    data.latitude = parseFloat(input.variables[latitude]),
-    data.longitude = parseFloat(input.variables[longitude]);
+    data.version = (input.bytes[0]).toString();
+    data.bat_low = ((input.bytes[1] << 8) | input.bytes[2]).toString() + " mV";
+    data.bat_low_count_max = (input.bytes[3]).toString();
+    data.bat_low_min = ((input.bytes[4] << 8) | input.bytes[5]).toString() + " mV";
   }
 
   data.latitude = parseFloat(input.variables["latitude"]);
