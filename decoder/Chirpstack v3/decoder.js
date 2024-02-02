76da633c55160b037771b289e4de7d5d12cf581e
Alexander Leisentritt <alexander.leisentritt@alitecs.de>
2024-02-02 08:25:54 +0100

    firmware to v1.2 and decoders for it

--- decoder/Chirpstack v3/decoder.js
+++ decoder/Chirpstack v3/decoder.js
@@ -6,13 +6,17 @@ function Decode(fPort, bytes, variables) {
     decoded.fence_positive = (bytes[2] << 8) | bytes[3];
     decoded.fence_negative = (bytes[4] << 8) | bytes[5];
   }
-  else if (fPort === 2 && bytes.length === 7) {
+  else if (fPort === 2 && bytes.length === 5) {
     decoded.version = bytes[0];
     decoded.tdc = (bytes[1] << 12) | (bytes[2] << 8) | bytes[3];
-    decoded.msr_ms = (bytes[4] << 8) | bytes[5];
-    decoded.max_volt = (bytes[6] << 8) | bytes[7];
+    decoded.daily_confirmed_uplinks = bytes[4];
   }
-  else if (fPort === 3 && bytes.length === 3) {
+  else if (fPort === 3 && bytes.length === 5) {
+    decoded.version = bytes[0];
+    decoded.max_volt = (bytes[1] << 8) | bytes[2];
+    decoded.msr_ms = (bytes[3] << 8) | bytes[4];
+  }
+  else if (fPort === 4 && bytes.length === 6) {
     decoded.version = bytes[0];
     decoded.bat_low = (bytes[1] << 8) | bytes[2];
     decoded.bat_low_count_max = bytes[3];
--- decoder/Chirpstack v4/decoder_loc.js
+++ decoder/Chirpstack v4/decoder_loc.js
@@ -9,10 +9,14 @@ function decodeUplink(input) {
   else if (input.fPort == 2) {
     data.version = (input.bytes[0]).toString();
     data.tdc = ((input.bytes[1] << 12) | (input.bytes[2] << 8) | input.bytes[3]).toString() + " s";
-    data.msr_ms = ((input.bytes[4] << 8) | input.bytes[5]).toString() + " ms";
-    data.max_volt = ((input.bytes[6] << 8) | input.bytes[7]).toString() + " V";
+    data.daily_confirmed_uplinks = (input.bytes[4]).toString();
   }
   else if (input.fPort == 3) {
+    data.version = (input.bytes[0]).toString();
+    data.max_volt = ((input.bytes[1] << 8) | input.bytes[2]).toString() + " V";
+    data.msr_ms = ((input.bytes[3] << 8) | input.bytes[4]).toString() + " ms";    
+  }
+  else if (input.fPort == 4) {
     data.version = (input.bytes[0]).toString();
     data.bat_low = ((input.bytes[1] << 8) | input.bytes[2]).toString() + " mV";
     data.bat_low_count_max = (input.bytes[3]).toString();
